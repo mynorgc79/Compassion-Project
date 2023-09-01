c75a18bcce654503833be761276f8748e0c19850
mynorgc79 <mynorguarchaj99@gmail.com>
2023-09-01 13:34:52 -0600

    add conexion database

--- compassion/compassion/settings.py
+++ compassion/compassion/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "compassion.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'compassion',
+        "USER": 'root',
+        "PASSWORD": 'gu-400',
+        "HOST": '127.0.0.1',
+        "PORT": '3306',
     }
 }
 
