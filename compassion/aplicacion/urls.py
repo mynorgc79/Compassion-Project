f44a097b6f5039c9ae09c6b1926aa5fff8717896
mynorgc79 <mynorguarchaj99@gmail.com>
2023-09-06 23:59:43 -0600

    Agregando plantillas para registro y listar

--- compassion/aplicacion/urls.py
+++ compassion/aplicacion/urls.py
@@ -1,6 +1,25 @@
+from django.contrib import admin
 from django.urls import path
+from django import views
 from .import views
 
+# importamos desde views
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.index, name='index'),
+    path('agregar', views.agregar, name='agregar'),
+    path('listar', views.listar, name='listar'),
+    path('actualizar', views.actualizar, name='actualizar'),
+    path('eliminar', views.eliminar, name='eliminar'),
+    path('buscar', views.buscar, name='buscar'),
+
+
+    # path('nosotros', views.nosotros, name='nosotros'),
+    # path('beneficiarios', views.beneficiarios, name='beneficiarios'),
+    # path('beneficiarios/crear', views.crearBeneficiario,
+    #    name='crearBeneficiario'),
+    # path('beneficiarios/editar', views.editarBeneficiario,
+    #    name='editarBeneficiario'),
+
+
+
 ]
--- compassion/aplicacion/views.py
+++ compassion/aplicacion/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates")',
+)
 
-def inicio(request):
-    return HttpResponse("<h1>Bienvenido Mynor</h1>")
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def agregar(request):
+    return render(request, 'crud-beneficiarios/agregar.html')
+
+
+def listar(request):
+    return render(request, 'crud-beneficiarios/listar.html')
+
+
+def actualizar(request):
+    return render(request, 'crud-beneficiarios/actualizar.html')
+
+
+def eliminar(request):
+    return render(request, 'crud-beneficiarios/eliminar.html')
+
+
+def buscar(request):
+    return render(request, 'crud-beneficiarios/buscar.html')
