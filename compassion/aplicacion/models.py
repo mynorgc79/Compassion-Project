5021dc3ac62792cc206b628dcf9b8ae09ea0c4ac
mynorgc79 <mynorguarchaj99@gmail.com>
2023-09-08 00:44:10 -0600

    feat: agregando entidad de familias a la DB

--- compassion/aplicacion/models.py
+++ compassion/aplicacion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Familias(models.Model):
+    id_familia = models.AutoField(primary_key=True)
+    apellido_familia = models.CharField(max_length=30, null=False)
+    direccion = models.CharField(max_length=50, null=True)
+    contacto = models.CharField(max_length=30, null=True)
+    cantidad_hijos = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'familias'
