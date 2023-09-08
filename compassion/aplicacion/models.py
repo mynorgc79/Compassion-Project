838daab69e7d11aee765f6b92fa9fd7d72f63cc3
mynorgc79 <mynorguarchaj99@gmail.com>
2023-09-08 01:39:35 -0600

    Agregando entidades: beneficiarios y salidas

--- compassion/aplicacion/models.py
+++ compassion/aplicacion/models.py
@@ -1,14 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 
 class Familias(models.Model):
     id_familia = models.AutoField(primary_key=True)
-    apellido_familia = models.CharField(max_length=30, null=False)
-    direccion = models.CharField(max_length=50, null=True)
-    contacto = models.CharField(max_length=30, null=True)
-    cantidad_hijos = models.IntegerField(null=True)
+    apellido_familia = models.CharField(
+        max_length=50, null=False, verbose_name='apellido Familia')
+    direccion = models.CharField(
+        max_length=50, null=True, verbose_name='direccion')
+    contacto = models.CharField(
+        max_length=30, null=True, verbose_name='contacto')
+    cantidad_hijos = models.IntegerField(
+        null=True, verbose_name='cantidad Hijos')
 
     class Meta:
         db_table = 'familias'
+
+
+class Beneficiarios(models.Model):
+    codigo_beneficiario = models.IntegerField(
+        primary_key=True, verbose_name='Codigo')
+    nombre = models.CharField(max_length=25, null=False, verbose_name='Nombre')
+    apellido = models.CharField(
+        max_length=25, null=False, verbose_name='Apellido')
+    fecha_nacimiento = models.DateField(
+        null=False, verbose_name='Fecha de nacimiento')
+    edad = models.IntegerField(null=False, verbose_name='E   dad')
+    genero = models.CharField(max_length=25, null=False, verbose_name='Genero')
+    nivel = models.IntegerField(null=False, verbose_name='Nivel')
+    observacion = models.CharField(
+        max_length=100, null=True, verbose_name='observacion')
+    id_familia = models.ForeignKey(Familias, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Beneficiarios'
+
+
+class salidas(models.Model):
+    id_salidas = models.AutoField(primary_key=True)
+    tipo_salida = models.CharField(
+        max_length=20, verbose_name='Tipo de Salida')
+    fecha_salida = models.DateField(
+        null=False, verbose_name='Fecha de salida')
+    motivo = models.CharField(
+        max_length=60, null='true', verbose_name='Motivo')
+    estado = models.BooleanField(default='True', verbose_name='Estado')
+    codigo_beneficiario = models.ForeignKey(
+        Beneficiarios, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'salidas'
